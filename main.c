141fbf82d06314af5aa6e967798e3745e8660b70
Alan Masutti <alan.masutti@gmail.com>
2023-10-22 11:02:23 +0200

    Get n from the user + routine1 draft

--- main.c
+++ main.c
@@ -21,8 +21,31 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+
+// Routine 1
+void routine1(float *A, float *B, float *C, int n, clock_t *t_loop){
+    clock_t t_start, t_stop;
+    t_start = clock();
+    for (int i = 0; i < n; i++){
+        C[i] = A[i] + B[i];
+    }
+    t_stop = clock();
+    *t_loop = t_stop - t_start;
+}
 
 int main(int argc, char const *argv[]){
+    int n;
+    printf("Routine 1\n\n Input size n:\n");
+    do{
+        scanf("%d", &n);
+        if (n < 0){
+            printf("Invalid input, please insert a positive integer!\n");
+        }
+    } while (n < 0);
     
+    
+
     return 0;
 }
